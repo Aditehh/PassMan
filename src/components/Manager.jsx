5846077f30b443c94a49c86ca9c3fad9b34470bd
aditehh <aditya.aryal888@gmail.com>
2025-07-06 20:04:06 +0545

    first

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -5,42 +5,45 @@ const Manager = () => {
         <>
             <div className="absolute top-0 z-[-2] h-full w-full bg-[rgba(226,221,221,0.93)] bg-[radial-gradient(rgba(226,221,221,0.93))] bg-[size:20px_20px]"></div>
             <div className='min-h-full flex flex-col items-center  px-4 '>
-                <div className=' p-8   text-white'>
+                <div className=' p-8 w-[80vw]  text-white'>
                     <div className='text-center mb-8'>
                         <h1 className='text-4xl font-bold text-gray-700 bg-clip-text mb-2'>
                             PassMan
                         </h1>
                         <p className='text-gray-900 font-light'>Won't let you forget</p>
                     </div>
-                    
+
                     <div className="space-y-6">
                         <div className='flex flex-col space-y-4'>
                             <div className='relative'>
-                                <input 
-                                    className="w-full bg-white/10 backdrop-blur-sm text-black border border-slate-600/50 rounded-lg px-4 py-3 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent transition-all duration-200 hover:bg-white/15" 
-                                    type="text" 
-                                    placeholder='example@gmail.com' 
+                                <input
+                                    className="w-full bg-white/10 backdrop-blur-sm text-black border border-slate-600/50 rounded-lg px-4 py-3 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent transition-all duration-200 hover:bg-white/15"
+                                    type="text"
+                                    placeholder='example@gmail.com'
                                 />
                             </div>
-                            
+
                             <div className='flex space-x-3'>
-                                <input 
-                                    className="flex-1 bg-white/10 backdrop-blur-sm text-black border border-slate-600/50 rounded-lg px-4 py-3 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent transition-all duration-200 hover:bg-white/15" 
-                                    type="text" 
-                                    placeholder='Username' 
+                                <input
+                                    className="flex-1 bg-white/10 backdrop-blur-sm text-black border border-slate-600/50 rounded-lg px-4 py-3 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent transition-all duration-200 hover:bg-white/15"
+                                    type="text"
+                                    placeholder='Username'
                                 />
-                                <input 
-                                    className="flex-1 bg-white/10 backdrop-blur-sm text-black border border-slate-600/50 rounded-lg px-4 py-3 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent transition-all duration-200 hover:bg-white/15" 
-                                    type="password" 
+                                <input
+                                    className="flex-1 bg-white/10 backdrop-blur-sm text-black border border-slate-600/50 rounded-lg px-4 py-3 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent transition-all duration-200 hover:bg-white/15"
+                                    type="password"
                                     placeholder='Password'
                                 />
                             </div>
                         </div>
+                        <button class="cursor-pointer px-5 py-2.5 bg-gradient-to-r from-[#b58e73] to-[#ba9273] text-white font-semibold rounded-lg shadow-md hover:opacity-90 transition duration-200">
+                            Add Password
+                        </button>
                     </div>
                 </div>
             </div>
             <div>
-                
+
             </div>
         </>
     )
