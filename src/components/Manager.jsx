5198385b150808cd5ab243a0670dcde1a6109703
aditehh <aditya.aryal888@gmail.com>
2025-07-08 19:20:00 +0545

    again and again

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -159,24 +159,26 @@ const Manager = () => {
                                             {passwordArray.map((item, index) => {
                                                 return <tr key={index}
                                                     className="hover:bg-gray-50/50 transition-colors duration-200">
-                                                    <td className="px-8 py-3 text-gray-900 font-medium flex items-center">
-                                                        <a href="{item.email}" target='_blank'>{item.email}
-                                                        </a>
-                                                        <div onClick={() => { copyText(item.email); notify(); }}>
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                fill="none"
-                                                                viewBox="0 0 24 24"
-                                                                strokeWidth={1.8}
-                                                                stroke="currentColor"
-                                                                className="w-5 h-5 py-0.5 px-0.5 cursor-pointer"
-                                                            >
-                                                                <path
-                                                                    strokeLinecap="round"
-                                                                    strokeLinejoin="round"
-                                                                    d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-                                                                />
-                                                            </svg>
+                                                    <td className="px-8 py-3 text-gray-900 font-medium ">
+                                                        <div className='flex items-center'>
+                                                            <a href="{item.email}" target='_blank'>{item.email}
+                                                            </a>
+                                                            <div onClick={() => { copyText(item.email); notify(); }}>
+                                                                <svg
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                    fill="none"
+                                                                    viewBox="0 0 24 24"
+                                                                    strokeWidth={1.8}
+                                                                    stroke="currentColor"
+                                                                    className="w-5 h-5 py-0.5 px-0.5 cursor-pointer"
+                                                                >
+                                                                    <path
+                                                                        strokeLinecap="round"
+                                                                        strokeLinejoin="round"
+                                                                        d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                                                                    />
+                                                                </svg>
+                                                            </div>
                                                         </div>
                                                     </td>
                                                     <td className="px-8 py-3 text-gray-700 ">
@@ -221,6 +223,28 @@ const Manager = () => {
                                                             </div>
                                                         </div>
                                                     </td>
+
+
+                                                    <td className="px-8 py-3 flex gap-5 text-gray-600 font-mono">
+                                                        <span>
+
+                                                            <svg
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                className="w-6 h-10 text-gray-500 hover:text-black-700 cursor-pointer transition duration-150"
+                                                                viewBox="0 0 24 24"
+                                                                fill="currentColor"
+                                                            >
+                                                                <path d="M9 3a1 1 0 0 0-1 1v1H5.5a.5.5 0 0 0 0 1H6v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6h.5a.5.5 0 0 0 0-1H16V4a1 1 0 0 0-1-1H9Zm1 2V4h4v1h-4ZM8 6h8v12a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1V6Zm2 2a.5.5 0 0 1 1 0v8a.5.5 0 0 1-1 0V8Zm3-.5a.5.5 0 0 1 1 0v9a.5.5 0 0 1-1 0v-9Z" />
+                                                            </svg>
+
+                                                        </span>
+
+                                                        <span>
+
+
+                                                        </span>
+                                                    </td>
+
                                                 </tr>
                                             })}
                                         </tbody>
